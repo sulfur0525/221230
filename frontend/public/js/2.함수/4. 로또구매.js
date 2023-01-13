4021dd649396bf053e06b9d24e8ee4c6f699c397
김지호 <sulfur0525@naver.com>
2023-01-13 16:32:30 +0900

    2023.01.13

--- frontend/public/js/2.함수/4. 로또구매.js
+++ frontend/public/js/2.함수/4. 로또구매.js
@@ -16,6 +16,8 @@ function btnDel(){
 	//해당 div에 공백으로 넣어줌 
 	document.querySelector('.btns').innerHTML = ''
 	document.querySelector('.btnDel').style.display = 'none'
+	list.splice(0)
+	추첨버튼출력()
 }
 
 // 선택한 숫자를 저장하는 배열선언
@@ -40,7 +42,7 @@ function btnClick( i ){
 	
 }
 
-//4. 추첨버튼 활성화 및 리스트 출력
+//4. 추첨버튼 활성화 및 리스트 출력[1. 구매취소 2. 숫자등록 3. 숫자제거 ]
 function 추첨버튼출력(){
 	if(list.length==6){
 		document.querySelector('.lotto').style.display = 'inline'
@@ -50,6 +52,67 @@ function 추첨버튼출력(){
 	document.querySelector('.numBox').innerHTML = list
 }
 
+//추첨번호 목록
+let 추첨번호목록 = []
+
+//5. 추첨하기 버튼을 클릭했을때 함수
+function lotto(){
+	//1. 컴퓨터가 난수 발생[6개 1~45사이의 수]
+	for(let i = 1 ; i<=6 ; i++){
+		//console.log(parseInt(Math.random()*45+1))
+		let 난수 = parseInt(Math.random()*45+1)
+		if(추첨번호목록.indexOf(난수)!=-1){i--}
+		else{추첨번호목록.push(난수)}
+	}
+	document.querySelector('.result').innerHTML = 추첨번호목록
+	
+	//1.
+	let cnt = 0
+	for(let i = 0 ; i<6 ; i++){
+		for(let j =0; j<6 ; j++){
+			if(list[i]==추첨번호목록[j]){
+				cnt++
+			}
+		}
+	}
+	
+	//2.
+	cnt = 0 
+	for(let i = 0 ; i<6 ; i++){
+		if(list.indexOf(추첨번호목록[i]!=-1)){cnt++}
+	}
+	
+	//3. 
+	cnt = 0
+	for(let value of 추첨번호목록)
+		if(list.includes(value)){cnt++}
+	//list.includes(value)는 결과가 참/거짓으로 나옴
+	
+	
+	let 등수 = ''
+	if(cnt==6){등수='1등'}
+	else if(cnt==5){등수='2등'}
+	else if(cnt==4){등수='3등'}
+	else if(cnt==3){등수='4등'}
+	else {등수='꽝'}
+	
+	document.querySelector('.등수').innerHTML = 등수
+	
+	console.log(cnt)
+}
+
+
+
+/*
+난수 만들기
+	1. Math 클래스 [ 미리 만들어둔 성계도 ]
+		1. random()함수 [math 클래스가 제공하는 미리 만들어둔 함수]
+		예시) 
+			Math.random()*45 : 0~45 사이의 실수 난수 발생			
+			Math.random()*45+1 : 1~45 사이의 실수 난수 발생
+		2. floor(): 소수점 버림 함수
+	2. parseInt() : 정수 변환 함수
+*/
 //1. 문자열 처리
 		/*let btns = '<button onclick="btnClick('+i+')">'+ i +'</button>'*/
 		//2. 백틱을 이용한 문자열 처리
--- frontend/public/js/과제/과제3/과제3추가.js
+++ frontend/public/js/과제/과제3/과제3추가.js
@@ -1,29 +1,36 @@
-console.log('JS실행')//확인용
 
 let contentArray = []
 
-
-function addContent(){
-	console.log('onLogin실행')//확인용
+function addContent(){	
 	let write = document.querySelector('.write')
 	let writeValue = write.value
 	//let write = document.querySelector('.write').value 하면 한번에 가능
-	console.log('방문록 써지나' + writeValue)//확인용
-	
+
 	contentArray.push( writeValue )
-	console.log('방문록 배열에 써지나' + contentArray)//확인용
 	
+	printCon()
+}
+
+function onDelete(dno){
+	contentArray.splice(dno,1)
+	printCon()
+}
+
+function printCon(){
 	let 테이블행 = 	'<tr>'+
 					'<th>'+'번호'+'</th>'+
 					'<th>'+'방문록'+'</th>'+
+					'<th>'+'비고'+'</th>'+
 				'</tr>'
-
+				
 	for(let i = 0 ; i<contentArray.length ; i++){
 
 	테이블행 +=  '<tr>'+
 					'<td>'+(i+1)+'</td>'+
 					'<td>'+contentArray[i]+'</td>'+
+					'<td>'+'<button type="button" onclick="onDelete('+i+')">삭제</button>'+'</td>'+
 				'</tr>'
 	}
 document.querySelector('.table').innerHTML = 테이블행	
 }
+
